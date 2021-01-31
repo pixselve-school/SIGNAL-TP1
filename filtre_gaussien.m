bfe6aae995275f8bc77717549c86eedc367c9b4b
Pixselve <pixselve@gmail.com>
2021-01-31 13:21:37 +0100

    feat(gaussian): gaussian filter

---
+++ filtre_gaussien.m
@@ -0,0 +1,14 @@
+function [imageResult] = filtre_gaussien(taille, sigma, imageInput)
+% Applique un filtre gaussien de taille "taille" et d'écart-type "sigma" à
+% l'image "imageInput"
+filtre=zeros(taille,taille);
+for i= 1:taille
+    for j= 1:taille
+        filtre(i,j)= (1 / (sigma * sqrt(2 * pi))) * exp(-(((i - ceil(N / 2))^2 + (j - ceil(N / 2))^2) / (2 * sigma^2)));
+    end
+end
+
+imageResult = conv2(imageInput, filtre, 'same');
+
+end
+
